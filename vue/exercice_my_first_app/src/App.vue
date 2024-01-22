509716fb611031db0c880fab3f8c9837a4a0ab41
adam <adam.doudeau2@gmail.com>
2024-01-22 10:13:27 +0100

    import exo

---
+++ vue/exercice_my_first_app/src/App.vue
@@ -0,0 +1,26 @@
+<script setup>
+/*  
+CE COMPOSANT RACINE APP RESTE EN MODE COMPOSITION API
+IMPORTER LES COMPOSANTS ICI 
+*/
+import CompoOpt from './component/componentOpt.vue'
+console.log(CompoOpt.data)
+</script>
+
+<template>
+  <h1>Oui</h1>
+  <header>
+    <div class="Lol">
+      <CompoOpt msg="MARCHE" />
+    </div>
+      
+      
+  </header>
+</template>
+
+<style scoped>
+.lol {
+  position: absolute;
+}
+
+</style>
---
+++ vue/exercice_my_first_app/src/component/componentOpt.vue
@@ -0,0 +1,29 @@
+<script>
+export default {
+    name: 'CompoOpet',
+    data() {
+        return {
+            count: 2
+        }
+        
+    },
+    props: {
+        msg: {
+            type: String,
+            required: false,
+            default: "Hello"
+        },
+    },
+    methods: {
+        plus() {
+            this.count = (this.count*5)%4532
+        }
+    }
+}
+</script>
+<template>
+    <h2>{{ count }}</h2>
+    <button @click="plus">More</button>
+    <p v-if="count>3500">Nique !</p>
+    <p v-else>Panique !!!</p>
+</template>
